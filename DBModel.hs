96bd926e6d82e105d7b6fe2a63a6463fb8899e74
Scott West <scott.gregory.west@gmail.com>
2012-05-17 16:46:30 +0200

    Initial commit.

---
+++ DBModel.hs
@@ -0,0 +1,102 @@
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE QuasiQuotes #-}
+
+{-# LANGUAGE OverloadedStrings #-}
+
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE EmptyDataDecls #-}
+{-# LANGUAGE FlexibleContexts #-}
+module DBModel where -- (BibEntry, bibTable, year, title) where
+
+import Database.Persist
+import Database.Persist.Sqlite
+import Database.Persist.TH
+
+data PublicationType = 
+  Journal | Conference | Book 
+  | Misc | Article | TechReport | PhdThesis deriving (Show, Read, Eq)
+
+-- data BibEntry = 
+--   BibEntry { bibTitle :: String
+--            , bibYear :: Int
+--            , bibAuthors :: [String]
+--            , bibPubType :: PublicationType
+--            }
+
+derivePersistField "PublicationType"
+
+share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persist|
+BibEntry
+   title String
+   year Int
+   authors [String]
+   pubType PublicationType
+   
+ReferenceComment
+  refer BibEntryKey
+  title String
+  text String
+|]
+
+type BibEntryKey = Key SqlPersist BibEntry
+
+-- import Database.HaskellDB.Query
+-- import Database.HaskellDB.PrimQuery
+-- import Database.HaskellDB.FieldType
+-- import Database.HaskellDB.Database
+-- import Database.HaskellDB.DBLayout
+-- import Database.HaskellDB.CodeGen
+
+-- instance ExprType PublicationType where
+--   fromHaskellType _ = (IntT, False)
+-- instance ShowConstant PublicationType where
+--   showConstant = IntegerLit . fromIntegral . fromEnum
+-- instance GetValue PublicationType where
+--   getValue insts s field = toEnum `fmap` getValue insts s field
+
+-- tbl = mkDBDirectTable "BibTable" 
+--   [ ("Title", [t|String|])
+--   , ("Year", [t|Int|])
+--   , ("PubType", [t|PublicationType|])
+--   , ("Authors", [t|String|])
+--   ]
+
+-- mkDBDirectTable "BibTable" 
+--   [ ("Title", [t|String|])
+--   , ("Year", [t|Int|])
+--   , ("PubType", [t|PublicationType|])
+--   , ("Authors", [t|String|])
+--   ]
+
+
+-- mkDBDirectField "title"   [t|String|]
+-- mkDBDirectField "year"    [t|Int|]
+-- mkDBDirectField "pubtype" [t|PublicationType|]
+-- mkDBDirectField "authors" [t|String|]
+
+-- type a :=> b = RecCons a b
+-- type a :< b = a :=> Expr b
+-- type a :+ b = a b
+
+-- infixr 2 :<
+-- infixr 2 :=>
+-- infixr 1 :+
+
+-- type End = RecNil
+
+-- type BibEntry 
+--      =  Title :< String
+--      :+ Year :< Int
+--      :+ Pubtype :< PublicationType
+--      :+ Authors :< String
+--      :+ End
+
+-- bibTable :: Table BibEntry
+-- bibTable = baseTable bibTableName 
+--            ( hdbMakeEntry Title 
+--              # hdbMakeEntry Year 
+--              # hdbMakeEntry Pubtype
+--              # hdbMakeEntry Authors)
+--   where bibTableName = "bibliography"
---
+++ Main.hs
@@ -0,0 +1,260 @@
+{-# LANGUAGE QuasiQuotes, TemplateHaskell, TypeFamilies, OverloadedStrings #-}
+{-# LANGUAGE GADTs, FlexibleContexts, EmptyDataDecls #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+module Main where
+
+import Control.Applicative
+import Control.Monad
+import Control.Monad.IO.Class (liftIO)
+
+import Data.Char
+import Data.List
+
+import qualified Text.BibTeX.Entry as Bibtex
+import Text.BibTeX.Format
+import qualified Text.BibTeX.Parse as Bibtex
+import Text.Parsec (many1)
+import Text.Parsec.String (parseFromFile)
+
+import Database.Persist
+import Database.Persist.Sqlite
+import Database.Persist.Store as DB
+
+import DBModel
+
+import Graphics.UI.Gtk
+
+dbConf = SqliteConf "references.sqlite" 1
+
+parseEntries :: FilePath -> IO [Bibtex.T]
+parseEntries file = do
+  entriesEi <- parseFromFile (many1 Bibtex.entry) file 
+  return $ case entriesEi of
+    Left e -> error $ show e
+    Right es -> es
+
+toBibEntry :: Bibtex.T -> BibEntry
+toBibEntry t = 
+  let pubType = 
+        case map toLower (Bibtex.entryType t) of
+          "inproceedings" -> Conference
+          "incollection" -> Journal
+          "misc" -> Misc
+          "book" -> Book
+          "article" -> Article
+          "techreport" -> TechReport
+          "phdthesis" -> PhdThesis
+          e -> error $ "toBibEntry: " ++ e
+      fields = Bibtex.fields t
+      Just authors = lookup "author" fields
+      title = case lookup "title" fields <|> lookup "author" fields of
+        Just t -> t
+        Nothing -> error $ show t ++ " has no title"
+      Just year = lookup "year" fields
+      clean = unwords . words . filter (/= '\n') . filter (/= '\r')
+  in BibEntry (clean title) (read year) (authorsToList authors) pubType
+
+uniqueBibEntries = do 
+  es <- parseEntries "bibfile.bib"
+  return (map toBibEntry es)
+
+populateDB = do
+  es <- uniqueBibEntries
+  runDB $ mapM_ DB.insert es
+
+main :: IO ()
+main = do
+  initGUI
+  
+  w <- windowNew
+  set w [ windowTitle := "MyCite"
+        , windowDefaultWidth := 800
+        , windowDefaultHeight := 600
+        ]
+  vbox <- vBoxNew False 0
+  containerAdd w vbox
+  
+  menuBar <- menu w
+  boxPackStart vbox menuBar PackNatural 1
+  
+  container <- refsContainer vbox
+  
+  entries <- fetchEntries
+  listStore <- listStoreNew entries
+  
+  treeView <- bibEntryTreeView listStore entries 
+  containerAdd container treeView
+  
+  widgetShowAll w
+
+  w `onDestroy` (saveStore listStore >> mainQuit)
+
+  mainGUI
+
+menu win = do
+  menuBar <- menuBarNew
+  fileItem <- menuItemNewWithMnemonic "_File"
+  menuShellAppend menuBar fileItem
+  
+  fileMenu <- menuNew
+  quitItem <- imageMenuItemNewFromStock stockQuit
+  menuShellAppend fileMenu quitItem
+  menuItemSetSubmenu fileItem fileMenu
+  
+  on quitItem menuItemActivate (widgetDestroy win)
+  
+  return menuBar
+
+saveStore listStore = do
+  entities <- listStoreToList listStore
+  runDB $ do
+    let saveEntity (Entity key val) = replace key val
+    mapM_ saveEntity entities
+
+
+bibEntryTreeView listStore entries = do  
+  treeView <- treeViewNewWithModel listStore
+  
+  let 
+    addColumn :: CellRendererTextClass renderer => 
+                 String -> Bool -> (BibEntry -> String) -> renderer -> IO ()
+    addColumn title resize extractor renderer = do
+      column <- treeViewColumnNew
+      treeViewColumnPackStart column renderer False
+      set column [ treeViewColumnTitle := title
+                 , treeViewColumnResizable := resize
+                 ]
+      treeViewAppendColumn treeView column
+      cellLayoutSetAttributes column renderer listStore
+        (\row -> [cellText := extractor (entityVal row)])
+      set renderer [cellTextEditable := False]
+        
+    textColumn title extractor = 
+      cellRendererTextNew >>= addColumn title True extractor
+
+    spinColumn title extractor = do
+      renderer <- cellRendererSpinNew
+      adjustment <- adjustmentNew 2000 0 3000 1 1 0
+      set renderer [ cellRendererSpinDigits := 0
+                   , cellRendererSpinAdjustment := adjustment
+                   ]
+      addColumn title False extractor renderer
+  
+    reactIter path = do
+      Just iter <- treeModelGetIter listStore path
+      let i = listStoreIterToIndex iter
+
+      win <- windowNew
+      widget <- refWidget listStore i
+      containerAdd win widget
+      widgetShowAll win
+  
+  textColumn "Title" bibEntryTitle
+  textColumn "Authors" (intercalate "; " . bibEntryAuthors)
+  spinColumn "Year" (show . bibEntryYear)
+  
+  on treeView rowActivated (\ path _col -> reactIter path)
+
+  return treeView
+
+pack container w = boxPackStart container w PackNatural 2
+
+data BibEntryWidget = 
+  BibEntryWidget 
+  { bibWidgetIdent :: BibEntryKey
+  , bibWidgetTitle :: Entry
+  , bibWidgetYear :: SpinButton
+  }
+
+
+data GlobalState = GlobalState { dirty :: Bool }
+
+leftAlignNew = alignmentNew 0 0 0 0.5
+
+bibEntryWidgetNew listStore i = do
+  bibEntity <- listStoreGetValue listStore i
+  let bibEntry@(BibEntry title year authors pub) = entityVal bibEntity
+      key = entityKey bibEntity
+
+  entry <- entryNew
+  entrySetText entry title
+  on entry editableChanged 
+    (do 
+        title' <- entryGetText entry
+        let bibEntry' = bibEntry {bibEntryTitle = title'}
+        listStoreSetValue listStore i (bibEntity {entityVal = bibEntry'})
+    )
+
+  adjustment <- adjustmentNew 0 0 2020 1 5 0
+  spin <- spinButtonNew adjustment 1.0 0
+  spinButtonSetValue spin (fromIntegral year)
+  on spin editableChanged 
+    (do 
+        year' <- spinButtonGetValueAsInt spin
+        let bibEntry' = bibEntry {bibEntryYear = year'}
+        listStoreSetValue listStore i (bibEntity {entityVal = bibEntry'})
+    )
+  
+  return $ BibEntryWidget key entry spin
+
+saveBibEntryWidget (BibEntryWidget key title year) = runDB $ do
+  title' <- liftIO $ entryGetText title
+  year' <- liftIO $ spinButtonGetValue year
+  
+  update key
+    [ BibEntryTitle =. title'
+    , BibEntryYear =. round year'
+    ]
+                                         
+
+
+refWidget listStore i = do
+  container <- tableNew 2 2 False
+  BibEntryWidget _ titleWidget yearWidget <- 
+    bibEntryWidgetNew listStore i
+  
+  let attach :: WidgetClass w => Int -> Int -> w -> IO ()
+      attach x y w = do 
+        align <- leftAlignNew
+        containerAdd align w
+        tableAttach container align x (x + 1) y (y + 1)
+                        [Fill] [Shrink] 5 0
+      labelNew' = labelNew . Just
+  
+  labels <- mapM labelNew' ["Title", "Year"]
+  
+  let widgets = [toWidget titleWidget, toWidget yearWidget]
+  
+  zipWithM_ (attach 0) [0..1] labels 
+  zipWithM_ (attach 1) [0..1] widgets
+
+  return container
+
+refsContainer win = do
+  scrollWin <- scrolledWindowNew Nothing Nothing
+  scrolledWindowSetPolicy scrollWin PolicyAutomatic PolicyAutomatic
+
+  container <- vBoxNew False 2
+  scrolledWindowAddWithViewport scrollWin container
+  
+  
+  containerAdd win scrollWin
+  return container
+
+runDB f = createPoolConfig dbConf >>= runPool dbConf f
+
+fetchEntries :: IO [Entity BibEntry]
+fetchEntries = do
+  runDB (runMigration migrateAll)
+  runDB (selectList [] [])
+  
+authorsToList :: String -> [String]
+authorsToList = reverse . go [] . words
+  where
+    go acc [] = acc
+    go acc ws = 
+      let (author, rest) = break (== "and") ws
+          acc' = unwords author : acc
+      in case rest of
+        [] -> acc'
+        _:rest' -> go acc' rest'
